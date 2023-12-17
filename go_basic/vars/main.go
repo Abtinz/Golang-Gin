e6a5a2ec179d585e6225e6b40ca7254ebe10ba7e
abtinz <abtinzand@gmail.com>
2023-12-17 13:18:26 +0330

    simple vars

---
+++ go_basic/vars/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var name = "abtin"
+	var age = 21
+	fmt.Println(name, age)
+	fmt.Printf("%T\n",age)
+	fmt.Printf("%T\n",name)
+
+	const zandi = true
+
+
+}
