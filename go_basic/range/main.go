161e2abcb8df91c06fdfa806cf70a46e20d607f2
Abtinz <abtinzandi@gmail.com>
2023-12-17 19:00:23 +0330

    no need to returned data just like python

--- go_basic/range/main.go
+++ go_basic/range/main.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main(){
 	ids := []int{1,2,33,21,45,66,745,3}
-	for i , id:=range ids{
-		fmt.Println(i,id)
+
+	//loop over array
+	for index , id:=range ids{
+		fmt.Println(index,id)
+	}
+
+	//no need to returned data just like python
+	for _ , id:=range ids{
+		fmt.Printf("id: %d\n",id)
 	}
 } 
