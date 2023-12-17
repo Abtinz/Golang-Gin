8df7bbe8bbf2e1c8d6197b7963e7f02520fc5161
abtinz <abtinzand@gmail.com>
2023-12-17 16:23:27 +0330

    function in go(tow args)

--- go_basic/function/main.go
+++ go_basic/function/main.go
@@ -6,7 +6,11 @@ func greeting(name string) string{
 	return "hi " + name
 }
 
+func sum(num1 , num2 int){
+	fmt.Println(num1 + num2)
+}
+
 func main(){
 	fmt.Println(greeting( "Abtin"))
-
+	sum(2 , 2 )
 }
