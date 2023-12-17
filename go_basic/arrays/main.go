704c58ae61dfc2a9b6ba90372d37aa15800f20ad
abtinz <abtinzand@gmail.com>
2023-12-17 16:33:11 +0330

    arrays with no pre sizing

--- go_basic/arrays/main.go
+++ go_basic/arrays/main.go
@@ -13,4 +13,10 @@ func main(){
 	new_array[0] = "Django"
 	fmt.Println(new_array)
 
+	//no pre sizing 
+	android := []string{"android","Kotlin", "Jetpack compose"}
+
+	fmt.Println(len(android)) //len
+	fmt.Println(android[0:2]) //range
+
 }
