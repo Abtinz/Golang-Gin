6ae4c10175c3f7988057b154f5bda16e0ef7b4ce
Abtinz <abtinzandi@gmail.com>
2023-12-17 17:57:15 +0330

    map delete

--- go_basic/map/main.go
+++ go_basic/map/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main(){
 
 	email := make(map[string] string)
+
+	//add map
 	email["Abtin"] = "abtinzandi@gmail.com"
 	email["Sarvin"] = "sarvinnami@gmail.com"
 	email["Hamid"] = "hamidrezazandi@gmail.com"
@@ -12,5 +14,11 @@ func main(){
 	fmt.Println(email)
 	fmt.Println(len(email))
 	fmt.Println(email["Abtin"])
+
+	email["fake"] = "fake@gmail.com"
+	fmt.Println(email)
+	//delete map
+	delete(email , "fake")
+	fmt.Println(email)
 
 }
