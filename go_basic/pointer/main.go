3aa49f3760e2f96d651a6c938ad5f02526d82014
Abtinz <abtinzandi@gmail.com>
2023-12-17 19:09:35 +0330

    change  value of pointing address

--- go_basic/pointer/main.go
+++ go_basic/pointer/main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main(){
-	
+
 	number := 1
 	pointer := &number
 
 	fmt.Println(number , pointer)
+
+	fmt.Println(*pointer) //read from pointer
+
+	//change  value of pointing address
+	*pointer = 10
+	fmt.Println(number)
+
 
 } 
