ad57cbaa4b94ec3e6c94b01b9185c6883e55fdb6
Abtinz <abtinzandi@gmail.com>
2024-01-28 12:27:22 +0330

    Patient

---
+++ IoT Hospital/model/users_model.go
@@ -0,0 +1,10 @@
+package model
+
+type Patient struct {
+	UserID   uint   `gorm:"primaryKey; autoIncrement"`
+	Username string `gorm:"unique"`
+	Password string
+}
+
+//why i did not put token here?
+//for high security principals in local server(my other dimension always follows hacking this poor dimension)
